f1a7a72a4dcec1d848321d943aefe0ee5a25cbc4
Andreas Eriksson <ae225ak@student.lnu.se>
2025-11-26 07:13:50 +0100

    Start working on test suite

--- java/practice/practice_with_mocked_tests/app/src/test/java/com/lab/SwedishSocialSecurityNumberTest.java
+++ java/practice/practice_with_mocked_tests/app/src/test/java/com/lab/SwedishSocialSecurityNumberTest.java
@@ -2,24 +2,36 @@ package com.lab;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+//public class SwedishSocialSecurityNumberTest {
+//
+//    private SSNHelper helper;
+//
+//    @BeforeEach
+//    public void setUp() {
+//        helper = new SSNHelper();
+//    }
+//
+//    @Test
+//    public void shouldAcceptValidSSN() throws Exception {
+//        SwedishSocialSecurityNumber ssn = new SwedishSocialSecurityNumber("900101-0017", helper);
+//
+//        assertEquals("90", ssn.getYear());
+//        assertEquals("01", ssn.getMonth());
+//        assertEquals("01", ssn.getDay());
+//        assertEquals("0017", ssn.getSerialNumber());
+//    }
+//}
+
+@ExtendWith(MockitoExtension.class)
 public class SwedishSocialSecurityNumberTest {
-    
-    private SSNHelper helper;
-    
-    @BeforeEach
-    public void setUp() {
-        helper = new SSNHelper();
-    }
-    
-    @Test
-    public void shouldAcceptValidSSN() throws Exception {
-        SwedishSocialSecurityNumber ssn = new SwedishSocialSecurityNumber("900101-0017", helper);
-        
-        assertEquals("90", ssn.getYear());
-        assertEquals("01", ssn.getMonth());
-        assertEquals("01", ssn.getDay());
-        assertEquals("0017", ssn.getSerialNumber());
-    }
+
+    @Mock
+    private SSNHelper ssnHelper;
+
 }
